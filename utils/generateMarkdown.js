ce80e0137891eebb0514b1ff54ee1c2cdc324130
Melisa <andream_dearth@hotmail.com>
2024-02-05 11:53:26 +0000

    Adding the Readme template in generateMarjdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.name}
 
+  License: ${data.license}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License 
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contribute}
+
+  ## Tests
+  To run tests, run the following command:
+  ${data.test}
+
+  ## Questions
+  If you have any questions about this repo you can contact me directly at [${data.email}](mailto:${data.email}). Here you can find more of my work at [${data.username}](https://www.github.com/${data.username}).
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown; // makes it possible to export the function generateMarkdown to another file
