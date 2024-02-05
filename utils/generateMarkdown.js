2e8ee0d794112301d6357a6ed20f31b56c59d404
Melisa <andream_dearth@hotmail.com>
2024-02-05 22:30:16 +0000

    Working license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,20 @@
 // Object with badges URLs from shields.io
 const licenseBadges = {
-  "MIT License": "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
-  "Apache License 2.0": "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)",
-  "GNU General Public License v3.0": "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)",
-  "ISC License": "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+  "MIT": "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
+  "Apache": "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)",
+  "GPLv3": "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)",
+  "ISC": "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
   "None": "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)",
 }
 
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.name}
+function generateMarkdown(answers) {
+  return `# ${answers.name}
 
-  ${licenseBadges[data.license]}
+  ${licenseBadges[answers.license]}
 
   ## Description
-  ${data.description}
+  ${answers.description}
 
   ## Table of Contents
   1. [Installation](#installation)
@@ -26,23 +26,23 @@ function generateMarkdown(data) {
 
   ## Installation
   To install the necessary dependencies, run the following command:
-  ${data.installation}
+  ${answers.installation}
 
   ## Usage
-  ${data.usage}
+  ${answers.usage}
 
   ## License 
-  This project is licensed under the ${data.license} license.
+  This project is licensed under the ${answers.license} license.
 
   ## Contributing
-  ${data.contribute}
+  ${answers.contribute}
 
   ## Tests
   To run tests, run the following command:
-  ${data.test}
+  ${answers.test}
 
   ## Questions
-  If you have any questions about this repo you can contact me directly at [${data.email}](mailto:${data.email}). Here you can find more of my work at [${data.username}](https://www.github.com/${data.username}).
+  If you have any questions about this repo you can contact me directly at [${answers.email}](mailto:${answers.email}). Here you can find more of my work at [${answers.username}](https://www.github.com/${answers.username}).
 `;
 }
 
